9adb9a214fb24c4dc6aee5f273334e82cf7d6d66
Matt <undecidabot@gmail.com>
2020-05-22 16:32:24 +0800

    feature: Save combos

--- assets/main.js
+++ assets/main.js
@@ -1,50 +1,24 @@
 "use strict";
 
-const assetsPath = "assets/stardew/"
+const assetsPath = "assets/stardew/";
+const storageKey = "stardew-combos";
 
 const shedWidth = 390;
 const shedHeight = 437;
 
+const previewArea = document.getElementById("preview-area");
+const previewCanvas = document.getElementById("preview-canvas");
+const saveButton = document.getElementById("save-button");
+const unsaveButton = document.getElementById("unsave-button");
+const wallpaperContents = document.getElementById("wallpaper-contents");
+const flooringContents = document.getElementById("flooring-contents");
+const combosContents = document.getElementById("combos-contents");
+
 let selectedWallpaper = undefined;
 let selectedFlooring = undefined;
-
+let combos = [];
 let images = Object.create(null);
-
-let previewRegion = document.getElementById("preview-region");
-let previewArea = document.getElementById("preview-area");
-let previewCanvas = document.getElementById("preview-canvas");
-let selectionRegion = document.getElementById("selection-region");
-let wallpaperArea = document.getElementById("wallpaper-area");
-let wallpaperContents = document.getElementById("wallpaper-contents");
-let flooringArea = document.getElementById("flooring-area");
-let flooringContents = document.getElementById("flooring-contents");
-
-function select(wallpaperId, flooringId) {
-    if (wallpaperId !== null && wallpaperId !== selectedWallpaper) {
-        if (selectedWallpaper !== undefined) {
-            document.getElementById("wallpaper:" + selectedWallpaper).classList.remove("selected");
-        }
-
-        selectedWallpaper = wallpaperId;
-        document.getElementById("wallpaper:" + selectedWallpaper).classList.add("selected");
-    }
-
-    if (flooringId !== null && flooringId !== selectedFlooring) {
-        if (selectedFlooring !== undefined) {
-            document.getElementById("flooring:" + selectedFlooring).classList.remove("selected");
-        }
-
-        selectedFlooring = flooringId;
-        document.getElementById("flooring:" + selectedFlooring).classList.add("selected");
-    }
-}
-
-function imageLoadPromise(imageElement) {
-    return new Promise((resolve, reject) => {
-        imageElement.addEventListener("load", () => { resolve(); });
-        imageElement.addEventListener("error", () => { reject(); });
-    });
-}
+let imageDataCache = Object.create(null);
 
 function setup() {
     let loadingImages = [];
@@ -53,7 +27,9 @@ function setup() {
 
     previewCanvas.width = shedWidth;
     previewCanvas.height = shedHeight;
-    previewArea.appendChild(previewCanvas);
+
+    saveButton.addEventListener("click", () => { saveCombo(selectedWallpaper, selectedFlooring); });
+    unsaveButton.addEventListener("click", () => { unsaveCombo(selectedWallpaper, selectedFlooring); });
 
     let shedBackground = document.createElement("img");
     shedBackground.src = assetsPath + "Shed_Inside.png";
@@ -75,6 +51,7 @@ function setup() {
 
     // == Load Handler == //
     Promise.all(loadingImages).then(() => {
+        loadCombosFromStorage();
         select("001", "01");
         drawShed();
         document.body.classList.remove("is-loading");
@@ -91,7 +68,7 @@ function setupWallpaperChoices(container) {
         id = "0".repeat(3 - id.length) + id;
 
         let choice = document.createElement("button");
-        choice.id = "wallpaper:" + id;
+        choice.id = "wallpaper-" + id;
         choice.classList.add("wallpaper-choice");
         choice.addEventListener("click", () => { select(id, null); drawShed(); });
         choice.addEventListener("mouseenter", () => { drawShed(id, undefined); });
@@ -110,6 +87,7 @@ function setupWallpaperChoices(container) {
 
         loadingImages.push(imageLoadPromise(icon));
         loadingImages.push(imageLoadPromise(tile));
+        images["wallpaper/icon/" + id] = icon;
         images["wallpaper/" + id] = tile;
     }
 
@@ -125,7 +103,7 @@ function setupFlooringChoices(container) {
         id = "0".repeat(2 - id.length) + id;
 
         let choice = document.createElement("button");
-        choice.id = "flooring:" + id;
+        choice.id = "flooring-" + id;
         choice.classList.add("flooring-choice");
         choice.addEventListener("click", () => { select(null, id); drawShed(); });
         choice.addEventListener("mouseenter", () => { drawShed(undefined, id); });
@@ -144,6 +122,7 @@ function setupFlooringChoices(container) {
 
         loadingImages.push(imageLoadPromise(icon));
         loadingImages.push(imageLoadPromise(tile));
+        images["flooring/icon/" + id] = icon;
         images["flooring/" + id] = tile;
     }
 
@@ -210,6 +189,126 @@ function drawFlooring(context, id) {
             context.drawImage(tile, xOffset + x * tileWidth, yOffset + y * tileHeight);
         }
     }
+}
+
+function select(wallpaperId, flooringId) {
+    let oldCombo = document.getElementById(`combos-${selectedWallpaper}-${selectedFlooring}`);
+    if (oldCombo != null) { oldCombo.classList.remove("selected"); }
+
+    let oldWall = document.getElementById("wallpaper-" + selectedWallpaper);
+    if (oldWall != null) { oldWall.classList.remove("selected"); }
+    let oldFloor = document.getElementById("flooring-" + selectedFlooring);
+    if (oldFloor != null) { oldFloor.classList.remove("selected"); }
+
+    if (wallpaperId != null) { selectedWallpaper = wallpaperId; }
+    document.getElementById("wallpaper-" + selectedWallpaper).classList.add("selected");
+    if (flooringId != null) { selectedFlooring = flooringId; }
+    document.getElementById("flooring-" + selectedFlooring).classList.add("selected");
+
+    let newCombo = document.getElementById(`combos-${selectedWallpaper}-${selectedFlooring}`);
+    let comboExists = newCombo != null;
+    if (comboExists) { newCombo.classList.add("selected"); }
+    saveButton.style.display = comboExists ? "none" : "";
+    unsaveButton.style.display = comboExists ? "" : "none";
+}
+
+function saveCombo(wallpaperId, flooringId) {
+    let choice = document.createElement("button");
+    choice.id = `combos-${wallpaperId}-${flooringId}`;
+    choice.classList.add("combos-choice");
+    choice.addEventListener("click", () => { select(wallpaperId, flooringId); drawShed(); });
+    choice.addEventListener("mouseenter", () => { drawShed(wallpaperId, flooringId); });
+    choice.addEventListener("mouseleave", () => { drawShed(); });
+    combosContents.appendChild(choice);
+
+    let wallpaperIcon = document.createElement("img");
+    wallpaperIcon.classList.add("wallpaper-icon");
+    wallpaperIcon.src = getImageData(images["wallpaper/icon/" + wallpaperId]);
+    choice.appendChild(wallpaperIcon);
+
+    let flooringIcon = document.createElement("img");
+    flooringIcon.classList.add("flooring-icon");
+    flooringIcon.src = getImageData(images["flooring/icon/" + flooringId]);
+    choice.appendChild(flooringIcon);
+
+    combos.push({ wallpaper: wallpaperId, flooring: flooringId });
+    saveCombosToStorage();
+
+    select(null, null);
+}
+
+function unsaveCombo(wallpaperId, flooringId) {
+    combosContents.removeChild(document.getElementById(`combos-${wallpaperId}-${flooringId}`));
+
+    let index = combos.findIndex((c) => c.wallpaper == wallpaperId && c.flooring == flooringId);
+    combos.splice(index, 1);
+    saveCombosToStorage();
+
+    select(null, null);
+}
+
+function loadCombosFromStorage() {
+    try {
+        combos = JSON.parse(localStorage.getItem(storageKey));
+    } catch {}
+
+    if (combos == null) {
+        combos = [];
+    }
+
+    for (let c of combos) {
+        let choice = document.createElement("button");
+        choice.id = `combos-${c.wallpaper}-${c.flooring}`;
+        choice.classList.add("combos-choice");
+        choice.addEventListener("click", () => { select(c.wallpaper, c.flooring); drawShed(); });
+        choice.addEventListener("mouseenter", () => { drawShed(c.wallpaper, c.flooring); });
+        choice.addEventListener("mouseleave", () => { drawShed(); });
+        combosContents.appendChild(choice);
+    
+        let wallpaperIcon = document.createElement("img");
+        wallpaperIcon.classList.add("wallpaper-icon");
+        wallpaperIcon.src = getImageData(images["wallpaper/icon/" + c.wallpaper]);
+        choice.appendChild(wallpaperIcon);
+    
+        let flooringIcon = document.createElement("img");
+        flooringIcon.classList.add("flooring-icon");
+        flooringIcon.src = getImageData(images["flooring/icon/" + c.flooring]);
+        choice.appendChild(flooringIcon);
+    }
+}
+
+function saveCombosToStorage() {
+    try {
+        localStorage.setItem(storageKey, JSON.stringify(combos));
+    } catch {}
+}
+
+function imageLoadPromise(imageElement) {
+    return new Promise((resolve, reject) => {
+        imageElement.addEventListener("load", () => { resolve(); });
+        imageElement.addEventListener("error", () => { reject(); });
+    });
+}
+
+function getImageData(imageElement) {
+    let src = imageElement.src;
+ 
+    if (location.protocol === "file:") {
+        return src;
+    }
+
+    if (!(src in imageDataCache)) {
+        let canvas = document.createElement("canvas");
+        canvas.width = imageElement.naturalWidth;
+        canvas.height = imageElement.naturalHeight;
+    
+        let context = canvas.getContext("2d");
+        context.drawImage(imageElement, 0, 0);
+
+        imageDataCache[src] = canvas.toDataURL();
+    }
+
+    return imageDataCache[src];
 }
 
 function centerPreview() {
