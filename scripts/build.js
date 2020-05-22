eda37729d92ee36d4e68571ee6deadf03912f42e
Matt <undecidabot@gmail.com>
2020-05-22 22:50:13 +0800

    bugfix: Platform detection bug

--- scripts/build.js
+++ scripts/build.js
@@ -3,7 +3,7 @@
 const fs = require("fs");
 const { spawnSync } = require('child_process');
 
-const isWin = require('os').platform().indexOf('win') != -1;
+const isWin = require('os').platform() === "win32";
 
 const buildDir = "public/";
 const assetsDir = "assets/";
